5ce5a6a56f2b466152f753e7f09a5a2eae9033b9
michalprzyl <michal.przyl@gmail.com>
2023-05-14 14:54:13 +0200

    almost done animation of swaping 2 values

--- script.js
+++ script.js
@@ -4,11 +4,12 @@ let backgroundColor;
 let rectColor;
 
 // values
-let values;
+let instances;
 let multiplier;
 
 // rectangles
 let rectangleWidth = 10;
+
 
 function getRandomMinMax(min, max) {
     return Math.random() * (max - min) + min;
@@ -32,19 +33,88 @@ function createRandomColor() {
     return randomColor;
 }
 
+function createInstances(howMany) {
+    let randomInstances = []
+    for (let i = 0; i < howMany; i++) {
+        const newValueValue = getRandomMinMax(0, 100);
+        const newInstance = new Value(
+            10 + i * rectangleWidth + 3 * i,
+            newValueValue
+        );
+        randomInstances.push(newInstance);
+    }
+    return randomInstances;
+}
+
 function setup() {
     multiplier = 5;
     backgroundColor = color(50, 168, 82);
     rectColor = color(createRandomColor())
     createCanvas(800, 600);
-    values = createRandomNumberList(50);
+    instances = createInstances(50);
+}
+
+function swapTwoValues(val1, val2){
+    const val1_x = val1.x;
+    const val2_x = val2.x;
+
+    while(val1.x != val2_x){
+        // we need to increase val1.x for one pixel in 
+        // direction of the second Value instance x
+        // val1.x += (val2_x - val1.x) > 0 ? 1 : -1;
+        if ((val2_x - val1.x) > 0) {
+            val1.x += 1;
+        }
+        else {
+            val1.x -= 1;
+        }
+    }
+}
+
+function keyPressed(){
+    if (key == 'a'){
+        // swapTwoValues(instances[0], instances[1]);
+        isSwapping = true;
+    }
+}
+let isSwapping = false;
+
+function changeValuesForSwap(val1, val2){
+    if (!isSwapping) {return;}
+    const val1_x = val1.x;
+    const val2_x = val2.x;
+
+    if(val1.x != val2_x){
+        // we need to increase val1.x for one pixel in 
+        // direction of the second Value instance x
+        // val1.x += (val2_x - val1.x) > 0 ? 1 : -1;
+        if ((val2_x - val1.x) > 0) {
+            val1.x += 1;
+        }
+        else {
+            val1.x -= 1;
+        }
+    }
+
+    if(val2.x != val1_x){
+        // we need to increase val1.x for one pixel in 
+        // direction of the second Value instance x
+        // val1.x += (val2_x - val1.x) > 0 ? 1 : -1;
+        if ((val2.x - val1_x) > 0) {
+            val2.x += 1;
+        }
+        else {
+            val2.x -= 1;
+        }
+    }
 }
 
 function draw() {
     background(backgroundColor);
-    for (let i = 0; i < values.length; i++) {
-        fill(rectColor);
-        rect(10 + i * rectangleWidth + i * 3, height - 10, rectangleWidth, -values[i] * multiplier);
+    fill(rectColor);
+    changeValuesForSwap(instances[0], instances[1]);
+    for (let i = 0; i < instances.length; i++) {
+        rect(instances[i].x, height - 10, rectangleWidth, -instances[i].value * multiplier);
     }
 }
 
